45100f2b5fffbf144b80fff27a8f94ec04d0bb72
frizio03 <fabrizio.tedeschi.scuola@gmail.com>
2024-05-23 16:28:56 +0200

    Added exercises 10

---
+++ Exercises/Exercises_10/EsameA.c
@@ -0,0 +1,239 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <time.h>
+#include <signal.h>
+
+// Definizione del TIPO pipe_t come array di 2 interi
+typedef int pipe_t[2];
+
+// Definizione del tipo linea di 255 char (da specifica)
+typedef char line_t[255];
+
+// Funzione per intercettare SIGPIPE
+void catch(int sig){
+    printf("Intercettato segnale SIGPIPE - tentativo di lettura da pipe processo figlio inesistente! ERRORE.\n");
+    exit(10);
+}
+
+// Definizione della funzione mia_random
+int mia_random(int n)
+{
+    int casuale;
+    casuale = rand() % n;
+    return casuale;
+}
+
+int main(int argc, char **argv)
+{
+    // Installazione funzione per segnale SIGPIPE
+    signal(SIGPIPE, catch);
+
+    // Dichiarazione variabili per processi
+    int pid, pidFiglio, status, ritorno;
+
+    // Dichiarazione variabili per file
+    int fd, fdout, len, rnd, index;
+    line_t linea;
+
+    // Controllo sul numero di parametri
+    if (argc < 6)
+    {
+        printf("Numero di parametri errato: argc = %d, ma dovrebbe essere >= 6\n", argc);
+        exit(1);
+    }
+
+    int H = atoi(argv[argc - 1]);
+    if (H <= 0 || H >= 255)
+    {
+        printf("Errore: l'ultimo parametro deve essere un numero compreso fra 0 e 255 esclusi\n");
+        exit(2);
+    }
+    printf("DEBUG: valore H = %d\n", H);
+
+    // Salvataggio del numero parametri passati da linea di comando
+    int N = argc - 2;
+
+    // Inizializzazione seme random
+    srand(time(NULL));
+
+    // Creazione del file fcreato
+    if ((fdout = creat("fcreato", 0644)) < 0)
+    {
+        printf("Errore nella creazione del file\n");
+        exit(3);
+    }
+
+    // Allocazione della memoria per array di pipe e controllo esito
+    pipe_t *piped_pf = calloc(N, sizeof(pipe_t));
+    pipe_t *piped_fp = calloc(N, sizeof(pipe_t));
+    if (piped_pf == NULL || piped_fp == NULL)
+    {
+        printf("Errore in calloc.\n");
+        exit(4);
+    }
+
+    // Creazione dei canali di pipe padre-figlio e controllo esito
+    for (int i = 0; i < N; i++)
+    {
+        if ((pipe(piped_pf[i])) < 0)
+        {
+            printf("Errore nella creazione della pipe.\n");
+            exit(5);
+        }
+
+        if ((pipe(piped_fp[i])) < 0)
+        {
+            printf("Errore nella creazione della pipe.\n");
+            exit(6);
+        }
+    }
+
+    // Generazione dei processi figli
+    for (int i = 0; i < N; i++)
+    {
+        // Esecuzione della fork e controllo esito
+        if ((pid = fork()) < 0)
+        {
+            printf("Errore in fork.\n");
+            exit(6);
+        }
+
+        if (pid == 0)
+        {
+            /* Codice del processo figlio */
+            //printf("DEBUG-Esecuzione processo FIGLIO con PID = %d\n", getpid());
+            
+            // Chiusura lati di lettura/scrittura inutilizzati
+            for (int k = 0; k < N; k++)
+            {
+                close(piped_fp[k][0]);
+                close(piped_pf[k][1]);
+                if (k != i)
+                {
+                    close(piped_pf[k][0]);
+                    close(piped_fp[k][1]);
+                }
+            }
+
+            // Apertura del file e controllo esito
+            if((fd = open(argv[i+1], O_RDONLY)) < 0)
+            {
+                printf("Errore nell'apertura del file '%s'.\n", argv[i+1]);
+                exit(-1);
+            }
+
+            // Lettura di ciascun carattere del file linea del file
+            int j = 0;
+            int stampe = 0;
+            while (read(fd, &(linea[j]), 1) != 0)
+            {
+                // Trovato il termine di una linea
+                if (linea[j] == '\n')
+                {
+
+                    // Comunicazione al padre della lunghezza di linea corrente (con terminatore)
+                    j++;
+                    write(piped_fp[i][1], &j, sizeof(j));
+
+                    // Lettura indice fornito dal padre
+                    read(piped_pf[i][0], &index, sizeof(index));
+
+                    // Controllo se l'indice ricevuto è ammissibile per la linea corrente
+                    if (index < j)
+                    {
+                        // Scrivo il carattere index della line acorrente su fcreato
+                        printf("DEBUG-Figlio di indice %d stampa '%c' trovato al posto %d\n", i, linea[index], index);
+                        
+                        write(fdout, &linea[index], 1);
+                        stampe++;
+                    }
+                    else{
+                        printf("DEBUG-Figlio di indice %d non ha caratteri al posto %d\n", i, index);
+                    }
+
+                    // Azzeramento contatore
+                    j = 0;
+                }
+                else
+                {
+                    // Aggiornamento indice
+                    j++;
+                }
+            }
+
+            //Comunicazione del numero di stampe al padre
+            exit(stampe);
+        }
+    }
+
+    /* Codice del processo padre */
+
+    // Chiusura lati di scrittura inutilizzati
+    for (int k = 0; k < N; k++)
+    {
+        close(piped_pf[k][0]);
+        close(piped_fp[k][1]);
+    }
+
+    // Per ciascuna delle linee dei file
+    for (int h = 0; h < H; h++)
+    {
+
+        rnd = mia_random(N - 1);
+
+        // Lettura dei valori dei figli per la len di linea corrente
+        for (int i = 0; i < N; i++)
+        {
+
+            // Lettura lunghezza linea da pipe
+            read(piped_fp[i][0], &len, sizeof(len));
+            printf("DEBUG-padre legge len = %d per la linea %d del processo i = %d\n", len, h, i);
+
+            // In caso di figlio presecelto considero la lunghezza
+            if (rnd == i)
+            {
+                index = mia_random(len - 1);
+            }
+        }
+
+        // Invio dei dati a ciascun figlio
+        for (int i = 0; i < N; i++)
+        {
+
+            // Comunicazione dell'indice del carattere da stampare
+            printf("DEBUG-padre comunica index = %d per la linea %d\n", index, h);
+            write(piped_pf[i][1], &index, sizeof(index));
+        }
+    }
+
+    // Attesa dei processi figli
+    for (int i = 0; i < N; i++)
+    {
+        // Esecuzione della wait e controllo esito
+        if ((pidFiglio = wait(&status)) < 0)
+        {
+            printf("Errore in wait eseguita dal padre\n");
+            exit(7);
+        }
+
+        // Controllo su terminazioni anomale del figlio e valore di ritorno
+        if (WIFEXITED(status) == 0)
+        {
+            printf("Figlio con PID = %d terminato in modo anomalo\n", pidFiglio);
+        }
+        else
+        {
+            ritorno = WEXITSTATUS(status);
+            printf("Figlio con PID = %d ha ritornato %d (255 = errori)\n", pidFiglio, ritorno);
+        }
+    }
+
+    // Uscita corretta dal programma
+    exit(0);
+}
---
+++ Exercises/Exercises_10/EsameB.c
@@ -0,0 +1,217 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <signal.h>
+
+// Definizione del TIPO pipe_t come array di 2 interi
+typedef int pipe_t[2];
+
+// Definizione funzione handler
+void catch (int sig)
+{
+    printf("DEBUG - processo con PID = %d, ha ricevuto l'indicazione a procedere.\n", getpid());
+}
+
+int main(int argc, char **argv)
+{
+    // Variabili per processi
+    int pidFiglio, status, ritorno;
+
+    // Variabili per gestione file
+    int fd, fdAF, nr;
+    char ch, chAF;
+
+    // Controllo sul numero di parametri
+    if (argc < 4)
+    {
+        printf("Numero di parametri errato: argc = %d, ma dovrebbe essere >= 4\n", argc);
+        exit(1);
+    }
+
+    // Salvataggio del numero parametri passati da linea di comando
+    int N = argc - 2;
+
+    // Apertura del file e controllo esito
+    if ((fdAF = open(argv[argc - 1], O_RDONLY)) < 0)
+    {
+        printf("Errore nell'apertura del file '%s'.\n", argv[argc - 1]);
+        exit(2);
+    }
+
+    // Allocazione array di pid
+    int *pid = calloc(N, sizeof(int));
+    int *terminato = calloc(N, sizeof(int));
+    if (pid == NULL || terminato == NULL)
+    {
+        printf("Errore in allocazione array di pid e controllo\n");
+        exit(3);
+    }
+
+    // Allocazione della memoria per array di pipe e controllo esito
+    pipe_t *piped = calloc(N, sizeof(pipe_t));
+    if (piped == NULL)
+    {
+        printf("Errore in calloc.\n");
+        exit(4);
+    }
+
+    // Creazione dei canali di pipe figlio-padre e controllo esito
+    for (int i = 0; i < N; i++)
+    {
+        if ((pipe(piped[i])) < 0)
+        {
+            printf("Errore nella creazione della pipe.\n");
+            exit(5);
+        }
+    }
+
+    // Generazione dei processi figli
+    for (int i = 0; i < N; i++)
+    {
+        // Esecuzione della fork e controllo esito
+        if ((pid[i] = fork()) < 0)
+        {
+            printf("Errore in fork.\n");
+            exit(6);
+        }
+
+        if (pid[i] == 0)
+        {
+            /* Codice del processo figlio */
+            printf("DEBUG-Esecuzione processo FIGLIO con PID = %d associato al file %s\n", getpid(), argv[i + 1]);
+
+            // Chiusura lati di lettura/scrittura inutilizzati
+            for (int k = 0; k < N; k++)
+            {
+                close(piped[k][0]);
+                if (k != i)
+                {
+                    close(piped[k][1]);
+                }
+            }
+
+            // Apertura del file e controllo esito
+            if ((fd = open(argv[i + 1], O_RDONLY)) < 0)
+            {
+                printf("Errore nell'apertura del file '%s'.\n", argv[i + 1]);
+                exit(-1);
+            }
+
+            while (1)
+            {
+                // Aggancio della funzione handler a SIGUSR1
+                signal(SIGUSR1, catch);
+                // Attesa del segnale di autorizzazione
+                pause();
+
+                // Lettura del carattere
+                nr = read(fd, &ch, 1);
+
+                // Uscita corretta dal ciclo per fine lettura
+                if (nr != 1)
+                    break;
+
+                // Comunicazione del carattere al padre
+                write(piped[i][1], &ch, 1);
+            }
+
+            exit(0);
+        }
+    }
+
+    /* Codice del processo padre */
+
+    // Chiusura lati di scrittura inutilizzati
+    for (int k = 0; k < N; k++)
+    {
+        close(piped[k][1]);
+    }
+
+    // Lettura di ciascun carattere del file associato al padre
+    while (read(fdAF, &chAF, 1) != 0)
+    {
+
+        // Invio autorizzazioni di lettura a tutti i figli non terminati
+        for (int i = 0; i < N; i++)
+        {
+            if (terminato[i] == 0)
+            {
+                // Attendo che il figlio sia pronto a ricevere il segnale
+                sleep(1);
+                if (kill(pid[i], SIGUSR1) < 0)
+                {
+                    printf("Errore in kill di autorizzazione\n");
+                }
+            }
+        }
+
+        // Lettura dei dati inviati da ciascun figlio
+        for (int i = 0; i < N; i++)
+        {
+
+            // Lettura da pipe di figli non terminati
+            if (terminato[i] == 0)
+            {
+                read(piped[i][0], &ch, 1);
+
+                // In caso di carattere diverso
+                if (ch != chAF)
+                {
+                    // Invio del segnale SIGKILL al figlio da terminare
+                    printf("DEBUG - Invio del segnale SIGKILL: ch != chAF\n");
+                    if (kill(pid[i], SIGKILL) < 0)
+                    {
+                        printf("Errore in kill di %d!\n", pid[i]);
+                    }
+                    terminato[i] = 1;
+                }
+            }
+        }
+    }
+
+    // Attendo che i figli attivi siano pronti a ricevere i segnali
+    sleep(1);
+
+    // Spedizione di un unltimo segnale per sbloccare i figli attivi per farli leggere EOF (ultimo ciclo)
+    for (int i = 0; i < N; i++)
+    {
+        if (terminato[i] == 0)
+        {
+            if (kill(pid[i], SIGUSR1) < 0)
+            {
+                printf("Errore in kill di autorizzazione\n");
+            }
+        }
+    }
+
+    // Attesa dei processi figli
+    for (int i = 0; i < N; i++)
+    {
+        // Esecuzione della wait e controllo esito
+        if ((pidFiglio = wait(&status)) < 0)
+        {
+            printf("Errore in wait eseguita dal padre\n");
+            exit(7);
+        }
+
+        // Controllo su terminazioni anomale del figlio e valore di ritorno
+        if ((status & 0xFF) != 0)
+        {
+            printf("Figlio con PID = %d terminato in modo anomalo (segnale)\n", pidFiglio);
+        }
+        else
+        {
+            ritorno = (status >> 8) & 0xFF;
+            printf("Figlio con PID = %d ha ritornato %d quindi il file '%s' risulta uguale a '%s'\n", pidFiglio, ritorno, argv[argc - 1], argv[i + 1]);
+        }
+    }
+
+    // Uscita corretta dal programma
+    exit(0);
+}
